0cc4e6c8533d8681b3abf72a988f1fb1d8dd77de
Kayode-Awe <kayode.awe@outlook.com>
2022-05-19 10:22:31 -0600

    Updated codes by completing TODOs.

---
+++ P02-VehiclesAPI/pricing-service/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO price (vehicle_id, currency, price) VALUES (1, 'USD', 3456.78);
+INSERT INTO price (vehicle_id, currency, price) VALUES (2, 'USD', 2556.78);
+INSERT INTO price (vehicle_id, currency, price) VALUES (3, 'USD', 4511.00);
+INSERT INTO price (vehicle_id, currency, price) VALUES (4, 'USD', 1278.55);
+INSERT INTO price (vehicle_id, currency, price) VALUES (5, 'USD', 3456.90);
